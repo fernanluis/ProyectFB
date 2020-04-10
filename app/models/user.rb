b81d379990132572a02b5ef31929e64602468718
fernanluis <luisfer439@hotmail.com>
2020-04-10 11:21:46 -0300

    Execution controller User (with index and show), devise:views, add first name and last name image fields to registration page, adding image uploads, add mount_uploader to User, and add another field to the registrationpage from allowing the uploading of images using javascript in views/devise/registrations/new.html.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+
+  # el método mount_uploader se agrega desde la gema carrierwave y el parámetro 'ImageUploader'
+  # suministrado hace referencia al nombre del cargador que se generó. Se usa 'ImageUploader'
+  # ya que generamos un cargador llamado image (app / uploaders / image_uploader.rb).
+  mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :picture_size # method into the validation requirements for the User model.
+
+  private
+    # Validates the size of an uploaded picture.
+    def picture_size
+      errors.add(:image, 'should be less than 1MB') if image.size > 1.megabytes
+    end
+
+  
 end
